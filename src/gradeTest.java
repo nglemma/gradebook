1cb10b04d092540b283f5c5400ac6e9f42aa5020
Emmanuel <ngeleemmanuel@yahoo.com>
2019-09-24 17:19:31 +0100

    new commit

---
+++ src/gradeTest.java
@@ -0,0 +1,13 @@
+
+import gradebook.Gradebook;
+
+
+public class gradeTest 
+{
+ 
+   public static void main(String args[])
+   {
+       Gradebook Cname = new Gradebook("java 101");
+       
+   }
+}
